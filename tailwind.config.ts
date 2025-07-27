cdd229f5501b93a486f6afc17007acde1d62244f
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2025-07-26 22:50:44 -0300

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+export default {
+  content: [
+    './src/pages/**/*.{js,jsx,tsx,mdx}',
+    './src/components/**/*.{js,jsx,tsx,mdx}',
+	],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
