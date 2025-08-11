89ccbc3c48ebeaacfc01f0bcdc67dc374979ea86
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2025-08-11 08:41:35 -0300

    install next-themes and insert it at pages/_app.tsx to different themes in feed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
-      <Component {...pageProps} />
-    </div>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
   )
 }
