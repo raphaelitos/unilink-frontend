0f7d09109ab6527714f283fcd5a19e76e63cc675
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2025-08-25 00:20:51 -0300

    updating configs with adress to images

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
+      { protocol: 'https', hostname: 'unilink-backend-production.up.railway.app', pathname: '/**' },
+      { protocol: 'http', hostname: 'localhost', port: '8080', pathname: '/**' },
     ],
   },
   reactStrictMode: true,
