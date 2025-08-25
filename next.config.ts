b1e08afb8ad1dddb957f6a6db0309748b6c88413
João Loss <joaoloss@Joaos-MacBook-Air.local>
2025-08-25 16:19:12 -0300

    fix: adding r2 url

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'unilink-backend-production.up.railway.app', pathname: '/**' },
       { protocol: 'http', hostname: 'localhost', port: '8080', pathname: '/**' },
+      { protocol: 'https', hostname: 'pub-2e1bb053d83a443db1e437521d180ac5.r2.dev', pathname: '/**' }
     ],
   },
   reactStrictMode: true,
